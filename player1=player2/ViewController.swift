69a4a2b63ae8d453a2940ab34ca19938bd49893d
yeshu <kanumuriyeswanth@gmail.com>
2015-12-23 10:55:37 -0500

    Initial Commit

---
+++ player1=player2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  player1=player2
+//
+//  Created by Yeswanth varma Kanumuri on 12/23/15.
+//  Copyright © 2015 Yeswanth varma Kanumuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
